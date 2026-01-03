8ab98a62a120ea8a2c7ed2ef555f139906bf87b0
Raj Vaibhav <rajvaibhav003@gmail.com>
2026-01-03 19:52:20 +0530

    Initial Commit

---
+++ File Compression Tool/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  File Compression Tool
+//
+//  Created by Raj Vaibhav on 03/01/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
